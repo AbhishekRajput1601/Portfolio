c2876ef258d35c08e86d6e2b04d7403c1f75568c
AbhishekRajput1601 <abhirajputofficial2000@gmail.com>
2025-08-10 18:27:49 +0530

    preset added into the cloudnary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,7 +7,8 @@ export async function uploadToCloudinary(file) {
   const url = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
   const formData = new FormData();
   formData.append('file', file);
-  formData.append('upload_preset', 'portfolio_upload'); // You may need to create this preset in Cloudinary
+  formData.append('upload_preset', 'portfolio_upload');
+  formData.append('folder', 'portfolio'); // Ensure images go to the 'portfolio' folder
 
   const response = await fetch(url, {
     method: 'POST',
